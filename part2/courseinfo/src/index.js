a3ed73b7b37417e6dcf95e7b8a6abfed95fd8493
Adil <aughr063@uottawa.ca>
2020-09-09 13:36:51 -0400

    courseinfo step5 complete

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercises 
+    + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
